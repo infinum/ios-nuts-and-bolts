9b9cea06f7521c2002642743f9d51c972891af92
Filip Gulan <gulan.filip@gmail.com>
2019-02-01 07:46:06 +0100

    Add initial catalog project.

---
+++ Catalog/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Catalog
+//
+//  Created by Filip Gulan on 01/02/2019.
+//  Copyright © 2019 Infinum. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Tests/CatalogTests.swift
@@ -0,0 +1,27 @@
+//
+//  CatalogTests.swift
+//  CatalogTests
+//
+//  Created by Filip Gulan on 01/02/2019.
+//  Copyright © 2019 Infinum. All rights reserved.
+//
+
+import XCTest
+@testable import Catalog
+
+class CatalogTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
