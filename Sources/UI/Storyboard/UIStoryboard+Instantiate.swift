5d84d0152b86ea49c38a7ae6a4ba879329a407e3
Filip Gulan <gulan.filip@gmail.com>
2019-02-01 08:30:01 +0100

    Add example sources.

---
+++ Sources/UI/Storyboard/UIStoryboard+Instantiate.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+public extension UIStoryboard {
+    
+    /// Instantiates and returns the view controller with the specified identifier and type.
+    /// If identifier is not specified then it uses type name as an identifier.
+    ///
+    /// - Parameters:
+    ///   - type: Concrete type of a view controller to instantiate
+    ///   - identifier: Identifier string that uniquely identifies the view controller in the storyboard
+    ///                 file - provide if different from type name
+    ///
+    /// - Returns: The view controller corresponding to the specified type and identifier string.
+    ///            If no view controller is associated with the string or type, this method throws an exception.
+    func instantiateViewController<T: UIViewController>(
+        ofType type: T.Type,
+        withIdentifier identifier: String? = nil
+    ) -> T
+    {
+        let identifier = identifier ?? String(describing: T.self)
+        return instantiateViewController(withIdentifier: identifier) as! T
+    }
+    
+}
