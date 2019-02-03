af45cd2f54341236fa616fa7158ae3e0fb7e328a
Filip Gulan <gulan.filip@gmail.com>
2019-02-03 23:19:24 +0100

    Add observable extensions.

---
+++ Sources/RxSwift/Observable/Observable+Progressable.swift
@@ -0,0 +1,50 @@
+//
+//  Observable+Progressable.swift
+//  Catalog
+//
+//  Created by Filip Gulan on 03/02/2019.
+//  Copyright © 2019 Infinum. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+public extension ObservableType {
+    
+    /// Shows loading on subscribe event.
+    public func handleShowLoading(with progressable: Progressable) -> Observable<E> {
+        return self
+            .do(onSubscribe: { [unowned progressable] in progressable.showLoading() })
+    }
+    
+    /// Hides loading on next event or error event.
+    public func handleHideLoading(with progressable: Progressable) -> Observable<E> {
+        return self
+            .do(onNext: { [unowned progressable] _ in progressable.hideLoading() })
+            .do(onError: { [unowned progressable] _ in progressable.hideLoading() })
+    }
+    
+    /// Shows loading on subscribe event and hides loading on next event or error event.
+    public func handleLoading(with progressable: Progressable) -> Observable<E> {
+        return self
+            .handleShowLoading(with: progressable)
+            .handleHideLoading(with: progressable)
+    }
+    
+    /// Shows failure on error event.
+    public func handleShowFailure(with progressable: Progressable) -> Observable<E> {
+        return self
+            .do(onError: { [unowned progressable] in progressable.showFailure(with: $0) })
+    }
+    
+    /// Shows loading on subscribe event, hides loading on next
+    /// event or error event and shows failure on error event.
+    public func handleLoadingAndError(with progressable: Progressable) -> Observable<E> {
+        return self
+            .handleLoading(with: progressable)
+            .handleShowFailure(with: progressable)
+    }
+    
+}
+
+
