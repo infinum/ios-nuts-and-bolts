1d4689ebc208f2ea31ce6861957c675de343e1af
Filip Gulan <gulan.filip@gmail.com>
2019-02-12 13:55:38 +0100

    Update tests.

--- Tests/Foundation/URL+AppendTests.swift
+++ Tests/Foundation/URL+AppendTests.swift
@@ -20,19 +20,26 @@ class URL_AppendTests: QuickSpec {
             it("should add query items to the basic url") {
                 let iPhone = URLQueryItem(name: "iphone", value: "x")
                 let iPad = URLQueryItem(name: "ipad", value: "pro")
-                let url = URL(string: "https://www.apple.com")!
+                let url = URL(string: "https://www.apple.com/devices")!
 
                 let newUrl = url.appendingQueryItems([iPhone, iPad])
-                expect(newUrl.absoluteString).to(equal("https://www.apple.com?iphone=x&ipad=pro"))
+                expect(newUrl.absoluteString).to(equal("https://www.apple.com/devices?iphone=x&ipad=pro"))
             }
 
             it("should append query items to the url with query items") {
                 let iPhone = URLQueryItem(name: "iphone", value: "x")
                 let iPad = URLQueryItem(name: "ipad", value: "pro")
-                let url = URL(string: "https://www.apple.com?ipod=nano")!
+                let url = URL(string: "https://www.apple.com/devices?ipod=nano")!
 
                 let newUrl = url.appendingQueryItems([iPhone, iPad])
-                expect(newUrl.absoluteString).to(equal("https://www.apple.com?ipod=nano&iphone=x&ipad=pro"))
+                expect(newUrl.absoluteString).to(equal("https://www.apple.com/devices?ipod=nano&iphone=x&ipad=pro"))
+            }
+
+            it("appending empty array should not change the url") {
+                let url = URL(string: "https://www.apple.com/devices?ipod=nano")!
+
+                let newUrl = url.appendingQueryItems([])
+                expect(newUrl.absoluteString).to(equal("https://www.apple.com/devices?ipod=nano"))
             }
 
          }
