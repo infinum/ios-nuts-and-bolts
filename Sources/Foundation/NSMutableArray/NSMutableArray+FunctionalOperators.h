3ec6c9ee34af0f5f4df679e084b2e19890da206c
Nikola Majcen <nikola.majcen@infinum.hr>
2019-05-19 21:02:39 +0200

    Added functional operators for NSMutableArray

---
+++ Sources/Foundation/NSMutableArray/NSMutableArray+FunctionalOperators.h
@@ -0,0 +1,47 @@
+//
+//  NSMutableArray+FunctionalOperators.h
+//  Catalog
+//
+//  Created by Nikola Majcen on 19/05/2019.
+//  Copyright © 2019 Infinum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSMutableArray<T: id> (FunctionalOperators)
+
+/**
+ Returns an mutable array containing the results of mapping the given
+ closure over the sequence's elements.
+ */
+- (NSMutableArray<id> *)map:(id _Nonnull (^)(T _Nonnull obj))block;
+
+/**
+ Returns an mutable array containing the concatenated results of calling
+ the given transformation with each element of this sequence.
+ */
+- (NSMutableArray<id> *)flatMap:(id (^)(T _Nonnull obj))block;
+
+/**
+ Calls the given closure on each element in the sequence
+ in the same order as @p for-in loop.
+ */
+- (void)forEach:(void (^)(T _Nonnull obj))block;
+
+/**
+ Returns an mutable array containing in order, the elements of the
+ sequence that satisfy the given predicate.
+ */
+- (NSMutableArray<T> *)filter:(BOOL (^)(T _Nonnull obj))block;
+
+/**
+ Returns the result of combining the elements of the sequence
+ using the given closure.
+ */
+- (id)reduce:(id)initial block:(id (^)(T _Nonnull acc, T _Nonnull obj))block;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sources/Foundation/NSMutableArray/NSMutableArray+FunctionalOperators.m
@@ -0,0 +1,69 @@
+//
+//  NSMutableArray+FunctionalOperators.m
+//  Catalog
+//
+//  Created by Nikola Majcen on 19/05/2019.
+//  Copyright © 2019 Infinum. All rights reserved.
+//
+
+#import "NSMutableArray+FunctionalOperators.h"
+
+@implementation NSMutableArray (FunctionalOperators)
+
+- (NSMutableArray *)map:(id  _Nonnull (^)(id _Nonnull))block
+{
+    if (!block) { return [self copy]; }
+    NSMutableArray *mutableArray = [[NSMutableArray alloc] init];
+    [self enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [mutableArray addObject:block(obj)];
+    }];
+    return [mutableArray copy];
+}
+
+- (NSMutableArray *)flatMap:(id  _Nonnull (^)(id _Nonnull))block
+{
+    if (!block) { return [self copy]; }
+    NSMutableArray *mutableArray = [[NSMutableArray alloc] init];
+    [self enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        id object = block(obj);
+        if ([object isKindOfClass:[NSArray class]]) {
+            NSArray *array = [object flatMap:block];
+            [mutableArray addObjectsFromArray:array];
+            return;
+        }
+        [mutableArray addObject:object];
+    }];
+    return [mutableArray copy];
+}
+
+- (void)forEach:(void (^)(id _Nonnull))block
+{
+    if (!block) { return; }
+    [self enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        block(obj);
+    }];
+}
+
+- (NSMutableArray *)filter:(BOOL (^)(id _Nonnull obj))block
+{
+    if (!block) { return [self copy]; }
+    NSMutableArray *mutableArray = [[NSMutableArray alloc] init];
+    [self enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if (block(obj)) {
+            [mutableArray addObject:obj];
+        }
+    }];
+    return [mutableArray copy];
+}
+
+- (id)reduce:(id)initial block:(id  _Nonnull (^)(id _Nonnull, id _Nonnull))block
+{
+    if (!block) { return initial; }
+    __block id object = initial;
+    [self enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        object = block(object, obj);
+    }];
+    return object;
+}
+
+@end
