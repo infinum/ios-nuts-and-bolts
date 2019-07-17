7ec1457473374a3a6b6cf99c73c9a7e21b8e8241
Jasmin Abou Aldan <jasmin.aboualdan@infinum.hr>
2019-07-17 14:11:40 +0200

    Move changes into the separate file

--- Sources/UI/UIView/UIView+Corners.swift
+++ Sources/UI/UIView/UIView+Corners.swift
@@ -8,40 +8,6 @@
 import UIKit
 
 public extension UIView {
-
-    /// Adds border color to the view. Should be used in combination with `borderWidth`.
-    ///
-    /// This modificator can be used in combination with `borderWidth` and `cornerRadius` modificators.
-    /// `borderColor` could be set from Storyboard.
-    @IBInspectable var borderColor: UIColor? {
-        get {
-            return layer.borderColor.flatMap(UIColor.init)
-        }
-        set (newColor) {
-            layer.borderColor = newColor?.cgColor
-        }
-    }
-
-    /// Adds border width to the view with color from `borderColor`.
-    ///
-    /// This modificator can be used in combination with `borderColor` and `cornerRadius` modificators.
-    /// `borderWidth` could be set from Storyboard.
-    @IBInspectable var borderWidth: CGFloat {
-        get { return layer.borderWidth }
-        set (newBorderWidth) { layer.borderWidth = newBorderWidth }
-    }
-
-    /// Adds corner radius to all corners of the view.
-    ///
-    /// This modificator can be used in combination with `borderColor` and `borderWidth` modificators.
-    /// `cornerRadius` could be set from Storyboard.
-    @IBInspectable var cornerRadius: CGFloat {
-        get { return layer.cornerRadius }
-        set (newCornerRadious) {
-            layer.masksToBounds = true
-            layer.cornerRadius = newCornerRadious
-        }
-    }
     
     /// Adds rounded corner edges to the view on specified coners with
     /// same radius for all of them.
