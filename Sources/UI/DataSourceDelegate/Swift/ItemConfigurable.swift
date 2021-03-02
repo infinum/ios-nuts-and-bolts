59a11b754077b369a01f44d135c918baf651dd56
mcolja <martin.colja@outlook.com>
2021-03-02 15:33:11 +0100

    add comments

--- Sources/UI/DataSourceDelegate/Swift/ItemConfigurable.swift
+++ Sources/UI/DataSourceDelegate/Swift/ItemConfigurable.swift
@@ -13,5 +13,7 @@
 public protocol ItemConfigurable {
     associatedtype ConfigurationItem
 
+    /// Configures the implementee with the recieved item
+    /// - Parameter item: item containing configuration data
     func configure(with item: ConfigurationItem)
 }
