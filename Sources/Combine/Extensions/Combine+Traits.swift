8a88cc0916c4703a665baa5bbd086957b7768a73
Zvonimir Medak <zvonimir.medak@infinum.hr>
2022-09-15 10:34:24 +0200

    fix warnings

---
+++ Sources/Combine/Extensions/Combine+Traits.swift
@@ -0,0 +1,73 @@
+//
+//  Combine+Traits.swift
+//  Catalog
+//
+//  Created by Zvonimir Medak on 15.09.2022..
+//  Copyright © 2022 Infinum. All rights reserved.
+//
+
+import Combine
+import CombineExt
+
+@available(iOS 13.0, *)
+extension Publisher {
+
+    /// Converts current Publisher sequence to `Driver`, completing on error event.
+    ///
+    /// - Returns: Driver - completing on error event
+    func asDriverOnErrorComplete() -> CombineDriver<Output> {
+        ignoreFailure().asDriver()
+    }
+
+    /// Converts current Publisher sequence to `Signal`, completing on error event.
+    ///
+    /// - Returns: Signal - completing on error event
+    func asSignalOnErrorComplete() -> CombineSignal<Output> {
+        ignoreFailure().asSignal()
+    }
+}
+
+@available(iOS 13.0, *)
+extension Publisher where Failure == Never {
+
+    /// Converts current Publisher sequence to a `Driver`. Events are received on the `Main` queue, the sequence is `shared` and `replayed`.
+    ///
+    /// - Returns: A `Driver` publisher.
+    func asDriver() -> CombineDriver<Output> {
+        eraseToAnyPublisher()
+            .receive(on: DispatchQueue.main)
+            .share(replay: 1)
+    }
+
+    /// Converts current Publisher sequence to a `Signal`. Events are received on the `Main` queue, the sequence is `shared`.
+    ///
+    /// - Returns: A `Signal` publisher.
+    func asSignal() -> CombineSignal<Output> {
+        eraseToAnyPublisher()
+            .receive(on: DispatchQueue.main)
+            .share(replay: 0)
+    }
+}
+
+@available(iOS 13.0, *)
+extension ShareReplayPublisher {
+
+    static func just(_ value: Output, replay: Bool = true) -> CombineDriver<Output> {
+        guard replay else {
+            return AnyPublisher.just(value).asSignal()
+        }
+        return AnyPublisher.just(value).asDriver()
+    }
+}
+
+@available(iOS 13.0, *)
+typealias ShareReplayPublisher<Output> = Publishers.Autoconnect
+<Publishers.Multicast
+<Publishers.ReceiveOn
+<AnyPublisher<Output, Never>, DispatchQueue>, ReplaySubject<Output, Never>>>
+
+@available(iOS 13.0, *)
+typealias CombineDriver<Output> = ShareReplayPublisher<Output>
+
+@available(iOS 13.0, *)
+typealias CombineSignal<Output> = ShareReplayPublisher<Output>
--- Sources/Combine/Extensions/NotificationCenter/NotificationCenter+Publishers.swift
+++ Sources/Combine/Extensions/NotificationCenter/NotificationCenter+Publishers.swift
@@ -23,13 +23,13 @@ extension NotificationCenter {
 
 // MARK: - NotificationCenter publisher factory
 
-@available(iOS 13.0, *)
 /// Creates AnyPublisher publisher for a specified NSNotification.Name
 ///
 /// - Parameters:
 ///  - notification: Notification name of the notification which will be observed
 ///
 /// - Returns: AnyPublisher
+@available(iOS 13.0, *)
 private func makeNotificationCenterPublisher(for notification: NSNotification.Name) -> AnyPublisher<Void, Never> {
     NotificationCenter.default
         .publisher(for: notification)
