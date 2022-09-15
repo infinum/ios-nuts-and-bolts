2123597782f7411986a99445d0885687fb086063
Zvonimir Medak <zvonimir.medak@infinum.hr>
2022-09-15 12:34:49 +0200

    add new functions when async doesn't throw

--- Sources/Combine/Extensions/AnyPublisher+AsyncAwait.swift
+++ Sources/Combine/Extensions/AnyPublisher+AsyncAwait.swift
@@ -13,9 +13,11 @@ import CombineExt
 public extension AnyPublisher {
 
     /// Create a new publisher for the given async task, returning output and finishing or erroring in case of a failure. Work can be cancelled as well.
+    /// 
     /// - Parameters:
-    ///     - task: A work task that needs to be executed and converted into `AnyPublisher`.
-    ///     - cancellationHandler: A handler that is called if a publisher needs to cancel, used to terminate pending work.
+    ///   - task: A work task that needs to be executed and converted into `AnyPublisher`.
+    ///   - cancellationHandler: A handler that is called if a publisher needs to cancel, used to terminate pending work.
+    ///
     /// - Returns: An `AnyPublisher` once the async task finished with `Output` constrained to the type of the returned value of the async task.
     static func createAsync(
         task: @escaping () async throws -> Output,
@@ -35,15 +37,54 @@ public extension AnyPublisher {
         }
     }
 
-    /// Create a new publisher for the given async task, returning output and finishing or erroring in case of a failure. Work can be cancelled as well.
+    /// Create a new publisher for the given async task, returning output and finishing. Work can be cancelled as well.
+    ///
     /// - Parameters:
-    ///     - task: A work task that needs to be executed and converted into `AnyPublisher`.
-    ///     - cancellationHandler: A handler that is called if a publisher needs to cancel, used to terminate pending work.
+    ///   - task: A work task that needs to be executed and converted into `AnyPublisher`.
+    ///   - cancellationHandler: A handler that is called if a publisher needs to cancel, used to terminate pending work.
+    ///
+    /// - Returns: An `AnyPublisher` once the async task finished with `Output` constrained to the type of the returned value of the async task.
+    static func createAsync(
+        task: @escaping () async -> Output,
+        cancellationHandler: (() -> Void)? = nil
+    ) -> AnyPublisher<Output, Failure> where Failure == Never {
+        return AnyPublisher.create { subscriber in
+            Task {
+                let output = await task()
+                subscriber.send(output)
+                subscriber.send(completion: .finished)
+            }
+            return AnyCancellable { cancellationHandler?() }
+        }
+    }
+
+    /// Create a new publisher for the given async task, returning output and finishing or erroring in case of a failure. Work can be cancelled as well.
+    ///
+    /// - Parameters:
+    ///  - task: A work task that needs to be executed and converted into `AnyPublisher`.
+    ///  - cancellationHandler: A handler that is called if a publisher needs to cancel, used to terminate pending work.
+    ///
     /// - Returns: An `AnyPublisher` once the async task finished with `Output` constrained to the type of the returned value of the async task. Error will not be thrown in this case.
     static func createAsyncResult(
         task: @escaping () async throws -> Output,
         cancellationHandler: (() -> Void)? = nil
     ) -> AnyPublisher<Result<Output, Failure>, Never> where Failure == Error {
+        return AnyPublisher
+            .createAsync(task: task, cancellationHandler: cancellationHandler)
+            .mapToResult()
+    }
+
+    /// Create a new publisher for the given async task, returning output and finishing. Work can be cancelled as well.
+    ///
+    /// - Parameters:
+    ///  - task: A work task that needs to be executed and converted into `AnyPublisher`.
+    ///  - cancellationHandler: A handler that is called if a publisher needs to cancel, used to terminate pending work.
+    ///
+    /// - Returns: An `AnyPublisher` once the async task finished with `Output` constrained to the type of the returned value of the async task.
+    static func createAsyncResult(
+        task: @escaping () async -> Output,
+        cancellationHandler: (() -> Void)? = nil
+    ) -> AnyPublisher<Result<Output, Failure>, Never> where Failure == Never {
         return AnyPublisher
             .createAsync(task: task, cancellationHandler: cancellationHandler)
             .mapToResult()
