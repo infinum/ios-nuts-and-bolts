be8cb034d5ba61c6a9eca4f60526edf02e602de7
Zvonimir Medak <zvonimir.medak@infinum.hr>
2022-09-15 09:56:18 +0200

    add NotificationCenter Publisher helpers

---
+++ Sources/Combine/Extensions/NotificationCenter/NotificationCenter+Publishers.swift
@@ -0,0 +1,38 @@
+//
+//  NotificationCenter+Publishers.swift
+//  Catalog
+//
+//  Created by Zvonimir Medak on 15.09.2022..
+//  Copyright © 2022 Infinum. All rights reserved.
+//
+
+import Foundation
+import Combine
+import UIKit
+
+@available(iOS 13.0, *)
+extension NotificationCenter {
+
+    // Example of makeNotificationCenterPublisher usage
+    var applicationDidBecomeActivePublisher: AnyPublisher<Void, Never> {
+        makeNotificationCenterPublisher(for: UIApplication.didBecomeActiveNotification)
+    }
+}
+
+// MARK: - Utility -
+
+// MARK: - NotificationCenter publisher factory
+
+@available(iOS 13.0, *)
+/// Creates AnyPublisher publisher for a specified NSNotification.Name
+///
+/// - Parameters:
+///  - notification: Notification name of the notification which will be observed
+///
+/// - Returns: AnyPublisher
+private func makeNotificationCenterPublisher(for notification: NSNotification.Name) -> AnyPublisher<Void, Never> {
+    NotificationCenter.default
+        .publisher(for: notification)
+        .mapToVoid()
+        .eraseToAnyPublisher()
+}
