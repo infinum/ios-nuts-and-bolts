ed329842e86ad97439f153a396684ffde0c0a806
Josip Cavar <josipcavar04@gmail.com>
2023-02-02 12:40:27 +0100

    Implement basic Realm setup with migrations
    - Fix swiftlint not running on M1

---
+++ Sources/Database/Realm/Database.swift
@@ -0,0 +1,60 @@
+import Foundation
+import RealmSwift
+import Realm
+import OSLog
+
+@available(iOS 14.0, *)
+class Database {
+    private(set) var realm: Realm!
+
+    init(configuration: Realm.Configuration? = nil) throws {
+        let config = try configuration ?? Database.defaultConfiguration()
+        if let fileURL = config.fileURL {
+            Logger.realm.debug("Database URL: \(fileURL)")
+        }
+        try DispatchQueue.database.sync { [unowned self] in
+            realm = try Realm(configuration: config, queue: DispatchQueue.database)
+        }
+    }
+}
+
+@available(iOS 14.0, *)
+extension Database {
+    /// Default realm configuration
+    /// Takes care of migrations, seeds and compacting if needed
+    static func defaultConfiguration() throws -> Realm.Configuration {
+        return Realm.Configuration(
+            fileURL: try Self.defaultURL(),
+            schemaVersion: SchemaVersion.current.rawValue,
+            migrationBlock: { migration, oldVersion in
+                SchemaVersion.execute(
+                    migration: migration,
+                    from: SchemaVersion(rawValue: oldVersion)!,
+                    to: .current
+                )
+            },
+            deleteRealmIfMigrationNeeded: false,
+            shouldCompactOnLaunch: { _, _ in false },
+            seedFilePath: try seedURL
+        )
+    }
+
+    static func defaultURL() throws -> URL {
+        try FileManager.default.url(
+            for: .documentDirectory,
+            in: .userDomainMask,
+            appropriateFor: nil,
+            create: true
+        )
+        .appendingPathComponent("database.realm")
+    }
+
+    /// Specify a path to `seedURL` in your bundle.
+    /// Use seed database only if you really need it.
+    static var seedURL: URL? {
+        get throws {
+            if FileManager.default.fileExists(atPath: try defaultURL().path) { return nil }
+            return nil
+        }
+    }
+}
---
+++ Sources/Database/Realm/SchemaVersion.swift
@@ -0,0 +1,67 @@
+import Foundation
+import RealmSwift
+import OSLog
+
+/// A list of Realm schema versions
+/// Write a comment above each schema version
+/// for easier lookup of what has changed
+enum SchemaVersion: UInt64, CaseIterable {
+    // Initial case
+    case v0
+    // Added property `x`
+    case v1
+}
+
+extension SchemaVersion: Comparable {
+    static func < (lhs: SchemaVersion, rhs: SchemaVersion) -> Bool { lhs.rawValue < rhs.rawValue }
+}
+
+extension SchemaVersion {
+    /// Current schema version
+    static var current: SchemaVersion { allCases.last! }
+}
+
+extension SchemaVersion {
+    @available(iOS 14.0, *)
+    static func execute(
+        migration: Migration,
+        from: SchemaVersion,
+        to: SchemaVersion,
+        migrations: [SchemaVersion: (Migration) -> Void] = Self.migrations
+    ) {
+        guard from < to else { return }
+        for schema in SchemaVersion.allCases where schema > from && schema <= to {
+            Logger.realm.log("Performing migration from \(from.rawValue) to \(to.rawValue)")
+            migrations[schema]?(migration)
+        }
+    }
+}
+
+private extension SchemaVersion {
+    /// A list of mappings from schema version to migration function
+    /// Only add to this list if you need to perform custom migration
+    /// If migration is automatically handled by Realm (e.g. adding a property)
+    /// For more, see:
+    /// https://www.mongodb.com/docs/realm/sdk/swift/model-data/change-an-object-model/#automatically-update-schema
+    static var migrations: [SchemaVersion: (Migration) -> Void] {
+        [
+            .v1: migrateToV1
+        ]
+    }
+}
+
+private extension SchemaVersion {
+
+    static func migrateToV1(migration: Migration) {
+        /**
+         Custom migration code.
+         E.g.
+         ```
+         migration.enumerateObjects(ofType: "Test") { oldObject, newObject in
+            newObject?["x"] = true
+            newObject?["playing"] = oldObject?.migrationValue(of: Bool.self, from: "notPlaying").map { !$0 } ?? true
+         }
+         ```
+         */
+    }
+}
