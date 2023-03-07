034a1c5e5b1aca180121165e9e4e407e9ebb7192
Josip Cavar <josipcavar04@gmail.com>
2023-03-07 12:17:32 +0100

    Add tests for DB operations

--- Tests/Database/Realm/DatabaseTests.swift
+++ Tests/Database/Realm/DatabaseTests.swift
@@ -11,4 +11,37 @@ class DatabaseTests: XCTestCase {
 
         XCTAssertNoThrow(try Database(configuration: configuration))
     }
+
+    func testCreateAndRead() async throws {
+        let database = try Database(configuration: .inMemory(name: name))
+
+        try await database.createOrUpdate(UserDB.self, with: User(id: "test", name: ""))
+
+        let user = try await database.read(UserDB.self, id: "test")
+        XCTAssertEqual(user.id, "test")
+    }
+
+    func testReadThrowsAnError() async throws {
+        let database = try Database(configuration: .inMemory(name: name))
+
+        await XCTAssertThrowsErrorAsync(try await database.read(UserDB.self, id: "test"))
+    }
+
+    func testReadOptionalReturnsNil() async throws {
+        let database = try Database(configuration: .inMemory(name: name))
+
+        let user = try await database.readOptional(UserDB.self, id: "test")
+        XCTAssertNil(user)
+    }
+
+    func testCreateAndThenUpdate() async throws {
+        let database = try Database(configuration: .inMemory(name: name))
+
+        try await database.createOrUpdate(UserDB.self, with: User(id: "test", name: "Initial"))
+        try await database.createOrUpdate(UserDB.self, with: User(id: "test", name: "Changed"))
+
+        let user = try await database.read(UserDB.self, id: "test")
+        XCTAssertEqual(user.id, "test")
+        XCTAssertEqual(user.name, "Changed")
+    }
 }
